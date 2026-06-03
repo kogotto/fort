694ee0fa5f7a7a5f2f57ad037f494b35545f345d
Yakov Didenko <kogotto@gmail.com>
2026-06-03 16:35:22 +0300

    Parse /proc/stat

--- client/main.c
+++ client/main.c
@@ -24,8 +24,68 @@ typedef struct {
     int coreCount;
 } CpuInfo;
 
+void printCoreInfo(CoreInfo* info) {
+    printf("user: %lu\n", info->user);
+    printf("nice: %lu\n", info->nice);
+    printf("system: %lu\n", info->system);
+    printf("idle: %lu\n", info->idle);
+    printf("iowait: %lu\n", info->iowait);
+    printf("irq: %lu\n", info->irq);
+    printf("softirq: %lu\n", info->softirq);
+    printf("steal: %lu\n", info->steal);
+    printf("guest: %lu\n", info->guest);
+    printf("guestNice: %lu\n", info->guestNice);
+}
+
+void printCpuInfo(CpuInfo* info) {
+    printf("All:\n");
+    printCoreInfo(&info->all);
+    printf("\n");
+    for (int core = 0; core < info->coreCount; ++core) {
+        printf("cpu%d:\n", core);
+        printCoreInfo(&info->cores[core]);
+        printf("\n");
+    }
+}
+
 #define SUCCESS 0
 #define CAN_NOT_OPEN_FILE 1
+#define CAN_NOT_READ_FIELD 2
+
+int readCpuStat(CoreInfo* coreInfo, FILE* file) {
+    if (EOF == fscanf(file, "%lu", &coreInfo->user)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->nice)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->system)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->idle)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->iowait)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->irq)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->softirq)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->steal)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->guest)) {
+        return CAN_NOT_READ_FIELD;
+    }
+    if (EOF == fscanf(file, "%lu", &coreInfo->guestNice)) {
+        return CAN_NOT_READ_FIELD;
+    }
+
+    return SUCCESS;
+}
 
 int readProcStat(CpuInfo* info) {
     FILE* file = fopen("/proc/stat", "r");
@@ -35,14 +95,32 @@ int readProcStat(CpuInfo* info) {
 
     char line[MAX_LINE];
     info->coreCount = 0;
-    printf("  before loop\n");
     while (!feof(file)) {
         fscanf(file, "%s", line);
+        if (strncmp(line, "cpu", 3)) {
+            continue;
+        }
         const int len = strlen(line);
-        printf("%d %s\n", len, line);
-        fgets(line, MAX_LINE - 1, file);
+        if (len == 3) {
+            const int res = readCpuStat(&info->all, file);
+            if (res != SUCCESS) {
+                fclose(file);
+                return res;
+            }
+            continue;
+        }
+
+        int n;
+        sscanf(line, "cpu%d", &n);
+        const int res = readCpuStat(&info->cores[n], file);
+        if (res != SUCCESS) {
+            fclose(file);
+            return res;
+        }
+        ++info->coreCount;
     }
 
+    fclose(file);
     return SUCCESS;
 
 }
@@ -52,10 +130,11 @@ int main() {
     CpuInfo info;
     switch (readProcStat(&info)) {
     case 0:
-        printf("success\n");
+        printCpuInfo(&info);
         break;
     default:
         printf("failure\n");
+        break;
     }
     return 0;
 }
