77018c385cda132926519da25f12561f759718e6
Yakov Didenko <kogotto@gmail.com>
2026-06-04 15:33:52 +0300

    Open UDP socket in net thread main function

--- client/main.c
+++ client/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdarg.h>
 
 #include <parse.h>
 #include <load.h>
@@ -8,8 +11,57 @@
 #define CAN_NOT_WRITE_CHARS 1
 #define BUFFER_OVERFLOW 2
 
+int syncPrint(const char* format, ...) {
+    va_list list;
+    va_start(list, format);
+    const int result = vprintf(format, list);
+    va_end(list);
+    return result;
+}
+
+typedef struct {
+    int socket;
+    struct addrinfo* info;
+} SocketInfo;
+
+SocketInfo tryOpenSocket(struct addrinfo* info) {
+    for (struct addrinfo* p = info; p != 0; p = p->ai_next) {
+        int socketFD = socket(info->ai_family, p->ai_socktype, 0);
+        if (socketFD != -1) {
+            SocketInfo result;
+            result.socket = socketFD;
+            result.info = p;
+            return result;
+        }
+    }
+    SocketInfo result;
+    result.socket = -1;
+    result.info = NULL;
+    return result;
+}
+
 void* netTreadMain(void* param) {
-    printf("Hello from net thread");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family =  AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    struct addrinfo* result;
+    int status;
+    if ((status = getaddrinfo("127.0.0.1", "1234", &hints, &result))) {
+        syncPrint("getaddrinfo failed: %s\n", gai_strerror(status));
+    }
+    syncPrint("getaddrinfo succeed\n");
+
+    SocketInfo info = tryOpenSocket(result);
+
+    if (info.socket == -1) {
+        syncPrint("Error: cannot open socket\n");
+    }
+    syncPrint("Soocket openning succeed\n");
+
+    close(info.socket);
+    freeaddrinfo(result);
     return NULL;
 }
 
@@ -39,10 +91,11 @@ int main() {
     for (int i = 0; i < load.coreCount; ++i) {
         printf(" %.2f", load.cores[i]);
     }
+    printf("\n");
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, netTreadMain, NULL);
+    pthread_t netThread;
+    pthread_create(&netThread, NULL, netTreadMain, NULL);
     void* res;
-    pthread_join(thread, &res);
+    pthread_join(netThread, &res);
     return 0;
 }
